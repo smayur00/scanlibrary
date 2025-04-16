d71d3c6bb2fa352b2f00de5c36a7383d80fc4ed3
smayur00 <smayur00@gmail.com>
2025-04-16 17:43:00 +0530

    Update ScanActivity.java

--- library/src/main/java/com/scanlibrary/ScanActivity.java
+++ library/src/main/java/com/scanlibrary/ScanActivity.java
@@ -96,11 +96,11 @@ public class ScanActivity extends Activity implements IScanner, ComponentCallbac
                    If the event is TRIM_MEMORY_COMPLETE, the process will be one of
                    the first to be terminated.
                 */
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.low_memory)
-                        .setMessage(R.string.low_memory_message)
-                        .create()
-                        .show();
+                // new AlertDialog.Builder(this)
+                //         .setTitle(R.string.low_memory)
+                //         .setMessage(R.string.low_memory_message)
+                //         .create()
+                //         .show();
                 break;
             default:
                 /*
